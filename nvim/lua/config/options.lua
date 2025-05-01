da1e46b2a9ed278d316fed255cf1f5006bb1d431
Austin Benavides <7328768+throwandgo@users.noreply.github.com>
2025-05-01 15:03:16 -0700

    Override default behavior for codeium
    Codeium is installed as a LazyVim extra, which is convenient, but its
    behavior out of the box adds Codeium suggestions to the nvim-cmp menu,
    which I don't like.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,3 +4,10 @@
 
 -- Disable all animations in Vim.
 vim.g.snacks_animate = false
+
+-- Disable showing AI-powered suggestions in the cmp menu.
+--
+-- The main frustration is that the AI-powered suggestions come back async, and they can end up
+-- taking the place of better suggestions from LSP and other sources in the menu. So, instead of
+-- showing them in the menu, we instead show them in the virtual text.
+vim.g.ai_cmp = false
