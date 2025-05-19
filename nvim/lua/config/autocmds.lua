ef8c9c42cc4ab0e43b5c987d37152693f4b43745
Austin Benavides <7328768+throwandgo@users.noreply.github.com>
2025-05-18 17:56:39 -0700

    Wrap git commit messages after 72 characters
    The guidelines recommend no more than 50 characters in the first line,
    and no more than 72 characters on any subsequent line.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Wrap at 72 characters it gitcommit buffers.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "gitcommit",
+  callback = function()
+    vim.opt_local.textwidth = 72
+    vim.opt_local.colorcolumn = "73" -- visual guide at 72
+  end,
+})
