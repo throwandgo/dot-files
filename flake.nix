59b2ca355fa967a153a325ac2cd7cc693638a2c3
Austin Benavides <7328768+throwandgo@users.noreply.github.com>
2025-10-10 14:17:37 -0700

    Upgrade to nixpkgs 25.05 and update packages

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.05-darwin";
     unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -40,10 +40,7 @@
           {
             home.username = username;
             home.homeDirectory = homeDirectory;
-            home.stateVersion = "24.05";
-
-            # Disable nixpkgs release check until nixpkgs 24.11 is released
-            home.enableNixpkgsReleaseCheck = false;
+            home.stateVersion = "25.05";
           }
         ];
 
